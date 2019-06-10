23079dc99d8bb771524a9732f6d4412eb3e525a9
jiamm <jiamingm@outlook.com>
2019-06-10 15:17:49 +0800

    Create 4_median_of_2arrs.go

---
+++ src/leetcode/4_median_of_2arrs.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main(){
+	nums1 := []int{1,2,3,6}
+	nums2 := []int{4,5,6,7}
+	result := findMedOf2Arrs(nums1,nums2)
+	fmt.Println(result)
+}
+
+
+
+
+
+func findMedOf2Arrs(nums1 []int, nums2 []int) float64 {
+	nums1 = append(nums1,nums2...)
+	sort.Ints(nums1)
+	nums1Len := len(nums1)
+	var res float64
+	if nums1Len %2 ==0{
+		temp := nums1Len/2
+		res = float64(nums1[temp-1]+nums1[temp])/2
+		return res
+	}
+	//
+	temp := nums1Len/2
+	res = float64(nums1[temp])
+	return res
+}
