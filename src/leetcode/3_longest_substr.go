b6dbe5808138e11c55141b422af1c3e8794a12f0
jiamm <jiamingm@outlook.com>
2019-06-10 14:56:01 +0800

    Create 3_longest_substr.go

---
+++ src/leetcode/3_longest_substr.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+
+func main(){
+	s := `aaabcddde`
+	start,max := lenOfLongestSubstring(s)
+	fmt.Println(start,max)
+}
+
+
+
+func lenOfLongestSubstring(s string) (int,int) {
+	m := make(map[rune]int)
+	start, max := -1, 0
+
+	for k, v := range s {
+		//repeated
+		if last, ok := m[v]; ok && last > start {
+			start = last
+		}
+		m[v] = k
+		//max
+		if k-start > max {
+			max = k - start
+		}
+	}
+	sindex := start-max
+	return sindex,max
+}
